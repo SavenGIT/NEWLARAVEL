7443a79a31b2487fc8b81bdb04e82e9f35348579
SavenGIT <139776672+SavenGIT@users.noreply.github.com>
2025-03-27 14:53:22 +0700

    Update Login.jsx

--- frontend/src/components/frontend/auth/Login.jsx
+++ frontend/src/components/frontend/auth/Login.jsx
@@ -1,9 +1,56 @@
 import React from "react";
-function Login(){
-    return(
-        <div>
-            <h1>Login Page</h1>
+
+function Login() {
+  return (
+    <>
+      <div className="container mt-5">
+        {/* Card Wrapper for the Box */}
+        <div className="card p-4 shadow">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="exampleInputEmail1" className="form-label">
+                Email address
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="exampleInputEmail1"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+              />
+              <small id="emailHelp" className="form-text text-muted">
+                We'll never share your email with anyone else.
+              </small>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="exampleInputPassword1" className="form-label">
+                Password
+              </label>
+              <input
+                type="password"
+                className="form-control"
+                id="exampleInputPassword1"
+                placeholder="Password"
+              />
+            </div>
+            <div className="mb-3 form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                id="exampleCheck1"
+              />
+              <label className="form-check-label" htmlFor="exampleCheck1">
+                Check me out
+              </label>
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
         </div>
-    );
+      </div>
+    </>
+  );
 }
+
 export default Login;
